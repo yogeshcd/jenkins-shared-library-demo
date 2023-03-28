a70c7c71aea99f53b1619184a44b61c4f50b6144
yogeshcd <gaikwadyogi2010@gmail.com>
2023-03-28 11:12:45 +0530

    Update sonarQualityCheck.groovy

--- vars/sonarQualityCheck.groovy
+++ vars/sonarQualityCheck.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) {
     withSonarQubeEnv(credentialsId: 'sonar-token') {
     // some block
-    // sh 'mvn clean package sonar:sonar
+    sh 'mvn clean package sonar:sonar
     echo "sonar quality check"
     }
 }
